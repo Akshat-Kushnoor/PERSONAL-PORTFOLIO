a11c00c90d08b0f2c1c25456a7b74431cb64bba5
Akshat kushnoor <akshatkushnoor@gmail.com>
2026-06-25 09:46:48 +0530

    removed backup . for functionality check .

--- backaup/src/components/SplineScene.jsx
+++
@@ -1,7 +0,0 @@
-import Spline from "@splinetool/react-spline";
-
-export default function SplineScene() {
-  return (
-    <Spline scene="https://prod.spline.design/JrK2xtL3DjxZmrnX/scene.splinecode" />
-  );
-}
--- backaup/src/sections/Projects.jsx
+++
@@ -1,6 +0,0 @@
-
-const Projects = () => {
-    return <div className="h-screen w-screen">Projects</div>;
-};
-
-export default Projects;
